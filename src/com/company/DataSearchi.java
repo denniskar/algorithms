668094059fa01f6d33d4f7c6f9d61512d962e1bd
Dennis Mwangi <dennismwangi@lanstar.co.ke>
2022-11-18 17:01:37 +0300

    palindrom checker

--- src/com/company/DataSearchi.java
+++ src/com/company/DataSearchi.java
@@ -1,10 +1,27 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DataSearchi {
 
     public static  void  main (String[]args){
-        String names ="denod # id pg rmmrmmr___88888 #";
-       String value=names.substring(0,names.lastIndexOf("_")).replace("_","");
-        System.out.println(value);
-    }
-}
+//        String names ="denod # id pg rmmrmmr___88888 #";
+//       String value=names.substring(0,names.lastIndexOf("_")).replace("_","");
+//        System.out.println(value);
+
+
+
+        String  num = "151";
+        StringBuilder ch = new StringBuilder();
+        for (int i = num.length()-1; i >= 0; i--) {
+            ch.append(num.charAt(i)) ;
+
+        }
+        if (num.equalsIgnoreCase(ch.toString()) ) {
+            System.out.println("it is palindrom");
+        } else {
+            System.out.println("it is not polindrom");
+        }
+
+}}
