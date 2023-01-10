82e931cb7cbe1382258307e85cacf559a980f1da
Dennis Mwangi <dennismwangi@lanstar.co.ke>
2023-01-10 16:52:48 +0300

    currency masking

--- src/com/company/Datess.java
+++ src/com/company/Datess.java
@@ -6,11 +6,13 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.Locale;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class Datess {
     public static void main(String[] args) throws ParseException {
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.ENGLISH);
         Date date = formatter.parse(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(LocalDateTime.now()));
-
+        AtomicReference<Double> interestTotals = new AtomicReference<>(4554543.0);
+        System.out.println(String.format(Locale.ENGLISH,"%1$,.2f",interestTotals.get()));
 
 }}
